22bdf40958b95a3e57645f8870e4f2c8cb6fc94b
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2022-09-10 23:29:40 +0000

    Sync LeetCode submission Runtime - 42 ms (6.20%), Memory - 14 MB (100.00%)

--- submissions/0870-magic-squares-in-grid/solution.py
+++ submissions/0870-magic-squares-in-grid/solution.py
@@ -1,29 +1,31 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        def isMagic(row, column):
-            rows = [grid[row + x][column: column + 3] for x in range(0,3)]
-            nums = []
-            [nums.extend(row) for row in rows]
-            if sorted(nums) != list(range(1, 10)):
+        def isMagic(arr):
+            if len(list(set(arr[0] + arr[1] + arr[2]))) != 9:
                 return False
-            columns = []
-            for i in range(0,3):
-                foo = []
-                for j in range(0,3):
-                    foo.append(grid[row + j][column + i])
-                columns.append(foo)
-                
-            diags = [
-                [grid[row][column], grid[row + 1][column + 1], grid[row + 2][column + 2]],
-                [grid[row][column + 2], grid[row + 1][column + 1], grid[row + 2][column]]
-            ]
-            s = sum(rows[0])
-            return all(sum(row) == s for row in rows) and all(sum(foo) == s for foo in columns) and all(sum(d) == s for d in diags)
+            
+            if not all([1 <= x and x <= 9 for x in arr[0] + arr[1] + arr[2]]):
+                return False
+             
+            return len(
+                    set(
+                        [
+                            sum(arr[0]),
+                            sum(arr[1]),
+                            sum(arr[2]),
+                            sum([x[0] for x in arr]),
+                            sum([x[1] for x in arr]),
+                            sum([x[2] for x in arr]),
+                            sum([arr[0][0], arr[1][1], arr[2][2]]),
+                            sum([arr[2][0], arr[1][1], arr[0][2]])
+                        ]
+                    )
+                ) == 1
         
         ret = 0
         for i in range(len(grid) - 2):
             for j in range(len(grid[0]) - 2):
-                if isMagic(i, j):
+                if (isMagic([x[j: j+3] for x in grid[i:i+3]])):
                     ret += 1
                     
         return ret
