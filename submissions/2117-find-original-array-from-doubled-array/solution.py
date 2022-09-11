2cebd7476cc3323f05a9502bbd89fad997588396
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2022-09-11 15:19:42 +0000

    Sync LeetCode submission Runtime - 1609 ms (5.08%), Memory - 32.8 MB (100.00%)

--- submissions/2117-find-original-array-from-doubled-array/solution.py
+++ submissions/2117-find-original-array-from-doubled-array/solution.py
@@ -1,31 +1,21 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        if not changed:
-            return []
-        
         if len(changed) % 2 != 0:
             return []
         
-        d = defaultdict(int)
-        for i in changed:
-            d[i] += 1
-            
-        if d[0] % 2 != 0:
+        if not changed:
             return []
-         
-        l = sorted(d.keys())
-        j = 0
+        
+        changed.sort()
+        vals = Counter(changed)
+        
         ret = []
-        for i in range(len(changed) // 2):
-            while d[l[j]] == 0:
-                j += 1
-            
-            if 2 * l[j] not in d or d[2 * l[j]] <= 0:
-                return []
-            else:
-                ret += [l[j]]
-                d[l[j]] -= 1
-                d[2 * l[j]] -= 1
+        for val in changed:
+            if vals[val] > 0 and vals[2 * val] > 0:
+                ret.append(val)
+                vals[val] -= 1
+                vals[2 * val] -= 1
                 
-                
-        return ret
+        return ret if all([x == 0 for x in vals.values()]) else []
+        
+        
