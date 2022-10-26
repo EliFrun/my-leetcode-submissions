a6678e8f972700ba1f068131c68ec722c5f14458
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2022-10-26 20:35:11 +0000

    Sync LeetCode submission Runtime - 75 ms (88.26%), Memory - 14.1 MB (100.00%)

--- submissions/0037-sudoku-solver/solution.py
+++ submissions/0037-sudoku-solver/solution.py
@@ -1,78 +1,70 @@
 class Solution:
-    def solveSudoku(self, final_board: List[List[str]]) -> None:
+    def valid_for_square(self, board, i, j):
+        if board[i][j] != '.':
+            return [board[i][j]]
+        
+        valid_values = set(list(range(1,10)))
+        for row in range(9):
+            if board[row][j] == '.':
+                continue
+            valid_values.discard(int(board[row][j]))
+            
+        for column in range(9):
+            if board[i][column] == '.':
+                continue
+            valid_values.discard(int(board[i][column]))
+            
+        for x in range(3):
+            for y in range(3):
+                if board[3 * (i // 3) + x][3 * (j // 3) + y] == '.':
+                    continue
+                valid_values.discard(int(board[3 * (i // 3) + x][3 * (j // 3) + y]))
+                
+        return [str(x) for x in list(valid_values)]
+    
+    
+    def solve(self, board):
+        new_board = copy.deepcopy(board)
+        updated = True
+        while(updated):
+            updated = False
+            for i in range(9):
+                for j in range(9):
+                    if new_board[i][j] != '.':
+                        continue
+                    possible_values = self.valid_for_square(new_board, i, j)
+                    if len(possible_values) == 0:
+                        return []
+                    if len(possible_values) == 1:
+                        new_board[i][j] = possible_values[0]
+                        updated = True
+            
+            
+        # done updating
+        # need to commit to values now
+        for i in range(9):
+            for j in range(9):
+                possible_values = self.valid_for_square(new_board, i, j)
+                if len(possible_values) >= 2:
+                    for val in possible_values:
+                        new_board[i][j] = val
+                        tmp = self.solve(new_board)
+                        if tmp != []:
+                            return tmp
+                    return []
+        
+        return new_board
+                    
+        
+    
+    
+    def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
         
-        def valid_for_square(b, x, y):
-            if b[x][y] != '.':
-                return [b[x][y]]
-            valid_values = set(list(range(1,10)))
-            for i in range(0, 9):
-                if b[x][i] != ".":
-                    valid_values.discard(int(b[x][i]))
-
-            for i in range(0, 9):
-                if b[i][y] != ".":
-                    valid_values.discard(int(b[i][y]))
-
-            for i in range(3):
-                for j in range(3):
-                    if b[3 * (x // 3) + i][3 * (y // 3) + j] != ".":
-                        valid_values.discard(int(b[3 * (x // 3) + i][3 * (y // 3) + j]))
-
-            return [str(x) for x in valid_values]
-
-        def solve(board):            
-            next_board = copy.deepcopy(board)
-            while (True):           
-                test_board = copy.deepcopy(next_board)
-                for i in range(9):
-                    for j in range(9):
-                        if next_board[i][j] != '.':
-                            continue
-                        possibilities = valid_for_square(next_board, i, j)
-                        if len(possibilities) == 1:
-                            next_board[i][j] = possibilities[0]
-                        elif len(possibilities) == 0:
-                            return []
-                
-                # reduce board to all forced values
-                if all(test_board[i][j] == next_board[i][j] for i in range(9) for j in range(9)):
-                    break
-                
-    
-            
-            # need to start committing to numbers
-            for i in range(9):
-                for j in range(9):
-                    if next_board[i][j] == '.':
-                        possibilities = valid_for_square(next_board, i, j)
-                        if len(possibilities) > 1:
-                            for val in possibilities:
-                                next_board[i][j] = val
-                                solution = solve(next_board)
-                                if solution != []:
-                                    return solution
-                            return []
-            
-            
-            # if above loop runs through all values and there all are not ".",
-            # the board must be solved
-            return next_board
-                  
-        
-        solved = solve(final_board)
-        
+        final_board = self.solve(board)
         for i in range(9):
             for j in range(9):
-                final_board[i][j] = solved[i][j]
-            
-            
-            
-                                            
-            
-            
-                                            
-                                        
-            
+                board[i][j] = final_board[i][j]
+        
