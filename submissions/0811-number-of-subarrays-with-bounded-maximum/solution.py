a9686ebe44ccf037406917f78ae1e97d693352ef
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-01-03 20:29:47 +0000

    Sync LeetCode submission Runtime - 70 ms (9.07%), Memory - 24.1 MB (82.40%)

--- submissions/0811-number-of-subarrays-with-bounded-maximum/solution.py
+++ submissions/0811-number-of-subarrays-with-bounded-maximum/solution.py
@@ -1,46 +1,34 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        lists = [[]]
-        for i in nums:
-            if i > right:
-                lists.append([])
-            else:
-                lists[-1].append(i)
-            
-        lists = [x for x in lists if x]
-        
-        def foo(l):
-            if max(l) < left:
-                return 0
-            ret = 0
-            i = 0
-            while i < len(l):
-                if l[i] >= left:
-                    ret += len(l) - i
-                else:
-                    j = i
-                    while j < len(l):
-                        if l[j] < left:
-                            j += 1
-                        else:
-                            ret += (j - i)*(len(l) - j)
-                            i = j - 1
-                            break
-                i += 1
-                        
-            return ret
-            
-        
-        ret = 0
-        for l in lists:
-            ret += foo(l)
-            
-        return ret
-        
-                
-                
-            
-                    
+        good_lists = []
+        floor = 0
+        for i in range(len(nums)):
+            if i == len(nums) - 1 and nums[i] <= right:
+                good_lists.append(nums[floor:i + 1])
+            if nums[i] > right:
+                if i != floor:
+                    good_lists.append(nums[floor:i])
+                floor = i + 1
 
-        
+        ret = 0
+        for l in good_lists:
+            bad_lists = []
+            floor = 0
+            for i in range(len(l)):
+                if i == len(l) - 1 and l[i] < left:
+                    bad_lists.append(l[floor:i + 1])
+                if l[i] >= left:
+                    if i != floor:
+                        bad_lists.append(l[floor:i])
+                    floor = i + 1
+
+            count = 0
+
+            for lis in bad_lists:
+                ll = len(lis)
+                count += ll * (ll + 1) / 2
+            n = len(l)
+            ret += n * ( n + 1 ) / 2 - count
+
+        return int(ret)
         
