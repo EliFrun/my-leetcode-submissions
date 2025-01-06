eaa06d9531c50ac85e4ffc9180baeb1af19fa659
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-01-06 04:27:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (78.22%)

--- submissions/1444-number-of-steps-to-reduce-a-number-to-zero/solution.py
+++ submissions/1444-number-of-steps-to-reduce-a-number-to-zero/solution.py
@@ -1,5 +1,11 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        if num == 0:
-            return 0
-        return 1 + self.numberOfSteps(num // 2 if num % 2 == 0 else num - 1)
+        count = 0
+        while(num > 0):
+            if num % 2 == 0:
+                num = num >> 1
+            else:
+                num -= 1
+            count += 1
+
+        return count
