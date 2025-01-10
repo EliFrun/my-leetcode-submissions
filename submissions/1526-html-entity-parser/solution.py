325e4b7c1ea13142f302a92128d5434b77037597
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-01-10 18:36:12 +0000

    Sync LeetCode submission Runtime - 19 ms (76.80%), Memory - 18 MB (94.85%)

---
+++ submissions/1526-html-entity-parser/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def entityParser(self, text: str) -> str:
+        return text.replace(
+                "&quot;", '"'
+            ).replace(
+                "&apos;", "'"
+            ).replace(
+                "&gt;", ">"
+            ).replace(
+                "&lt;", "<"
+            ).replace(
+                "&frasl;", "/"
+            ).replace(
+                "&amp;", "&"
+            )
+        
