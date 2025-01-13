6dc7dbfe25b87cec1488aaef041c3855d4c619e0
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-01-13 21:51:25 +0000

    Sync LeetCode submission Runtime - 7 ms (59.37%), Memory - 17.9 MB (78.45%)

--- submissions/0728-self-dividing-numbers/solution.py
+++ submissions/0728-self-dividing-numbers/solution.py
@@ -1,4 +1,13 @@
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        return [x for x in range(left, right + 1) if all(int(y) != 0 and x % int(y) == 0 for y in str(x))]
-        
+        def good(a):
+            for s in str(a):
+                if s == '0':
+                    return False
+                if a % int(s) != 0:
+                    return False
+
+            return True
+
+
+        return [x for x in range(left, right + 1) if good(x)]      
