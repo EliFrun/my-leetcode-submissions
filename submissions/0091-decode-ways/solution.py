10483f50f1879539cec5903a0ec43e1ff646f04d
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-01-22 15:54:20 +0000

    Sync LeetCode submission Runtime - 3 ms (26.74%), Memory - 18.2 MB (79.48%)

--- submissions/0091-decode-ways/solution.py
+++ submissions/0091-decode-ways/solution.py
@@ -1,30 +1,22 @@
 class Solution:
-    
     def numDecodings(self, s: str) -> int:
-        return self.helper({},s)
-    
-    
-    
-    def helper(self, memo, s):
-        if not s:
-            return 1
-        if s[0] == "0":
-            return 0
-        if len(s) == 1:
-            return 1
-        
-        ret = 0
-        if int(s[0:2]) <= 26:
-            if len(s) - 1 not in memo:
-                memo[len(s) - 1] = self.helper(memo, s[1:])
-            if len(s) - 2 not in memo:
-                memo[len(s) - 2] = self.helper(memo, s[2:])
-            ret += memo[len(s) - 1]
-            ret += memo[len(s) - 2]
-        else:
-            if len(s) - 1 not in memo:
-                memo[len(s) - 1] = self.helper(memo, s[1:])
-            ret += memo[len(s) - 1]
+
+        @functools.cache
+        def helper(st):         
+            if  len(st) == 0:
+                return 1
+
+            if st[0] == '0':
+                return 0
+
+            if len(st) == 1:
+                return 1
             
-        return ret
+            tmp = 0
+            if int(st[:2]) < 27:
+                tmp = helper(st[2:])
+
+            return helper(st[1:]) + tmp
+
+        return helper(s)
         
