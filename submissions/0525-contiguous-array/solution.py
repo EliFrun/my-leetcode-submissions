8822d81f2138615810a93d1e4021a94db3f6a7bf
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-01-24 14:01:53 +0000

    Sync LeetCode submission Runtime - 147 ms (5.30%), Memory - 24.2 MB (82.43%)

--- submissions/0525-contiguous-array/solution.py
+++ submissions/0525-contiguous-array/solution.py
@@ -1,25 +1,20 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        foo = [0]
-        for i in nums:
-            foo.append(foo[-1] + (1 if i == 1 else -1))
-            
-        d = defaultdict(lambda: [])
+        vals = [0]
+        for num in nums:
+            vals.append(vals[-1] + (1 if num == 1 else -1))
+
+        d = {}
+        for i, val in enumerate(vals):
+            d[val] = i
         
-        for i, v in enumerate(foo):
-            d[v].append(i)
-            
-            
-        curr_max = 0
-        if 0 in d:
-            curr_max = max(curr_max, d[0][-1])
-            
-        for _, v in d.items():
-            if len(v) == 1:
-                continue
-            
-            curr_max = max(curr_max, v[-1] - v[0])
-            
-        return curr_max
+        ret = 0
+        for i, val in enumerate(vals):
+            if val in d:
+                ret = max(ret, abs(i - d[val]))
+
+
+        return ret
+                
         
         
