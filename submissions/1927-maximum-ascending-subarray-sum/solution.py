47bff8a703fc8cbca9b27fc8894a3677564b24ac
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-02-04 02:55:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (81.29%)

---
+++ submissions/1927-maximum-ascending-subarray-sum/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        ret = 0
+        s = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                s += nums[i]
+            else:
+                ret = max(ret, s)
+                s = nums[i]
+
+        return max(ret, s)
+        
