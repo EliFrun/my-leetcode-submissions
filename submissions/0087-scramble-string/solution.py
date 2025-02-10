8184b4463b7cd828065f620db465b4ae273f7247
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-02-10 03:50:53 +0000

    Sync LeetCode submission Runtime - 43 ms (27.81%), Memory - 20.6 MB (47.25%)

--- submissions/0087-scramble-string/solution.py
+++ submissions/0087-scramble-string/solution.py
@@ -6,12 +6,14 @@ class Solution:
                 return s1 == s2
             if s1 == s2:
                 return True
+            if sorted(s1) != sorted(s2):
+                return False
 
             return any(
                 [ 
                     (
                         solve(s1[:i], s2[:i]) and solve(s1[i:], s2[i:]) or
-                        solve(s1[:i], s2[len(s1) - i:]) and solve(s1[i:], s2[:len(s1) - i])
+                        solve(s1[:i], s2[-i:]) and solve(s1[i:], s2[:-i])
                     )
                     for i in range(1, len(s1))
                 ]
