cfcbfa7e0f589784422ba55ec30278f9cbf8e6b1
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-03-03 13:59:28 +0000

    Sync LeetCode submission Runtime - 28 ms (68.14%), Memory - 34.5 MB (5.12%)

---
+++ submissions/2265-partition-array-according-to-given-pivot/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        left = []
+        middle = []
+        right = []
+
+        for n in nums:
+            if n < pivot:
+                left.append(n)
+            elif n > pivot:
+                right.append(n)
+            else:
+                middle.append(n)
+
+        return left + middle + right
+        
