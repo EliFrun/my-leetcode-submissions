967119582cde5bd7cee3d4cedcc86b4d6fb2ab36
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-16 18:52:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (83.64%)

--- submissions/0392-is-subsequence/solution.py
+++ submissions/0392-is-subsequence/solution.py
@@ -1,11 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        j = 0
-        for i in t:
-            if j == len(s):
-                break
-            if i == s[j]:
-                j += 1
+        i, j = 0, 0
+        while j < len(t):
+            if i == len(s):
+                return True
+            if t[j] == s[i]:
+                i += 1
+            j += 1
 
-        return j == len(s)
-            
+        return i == len(s)
+        
