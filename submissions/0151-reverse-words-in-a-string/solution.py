3052b344c5f2d8ad153cc05868399690d1da8b03
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-16 17:57:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (83.95%)

--- submissions/0151-reverse-words-in-a-string/solution.py
+++ submissions/0151-reverse-words-in-a-string/solution.py
@@ -1,9 +1,4 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.split(' ')
-        ret = ''
-        for word in reversed(words):
-            if word != ' ' and word != '':
-                ret += f'{word.strip()} '
-                
-        return ret[:-1]
+        return ' '.join([x.strip() for x in s.split(' ')[::-1] if x])
+        
