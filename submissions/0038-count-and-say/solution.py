953063b54d45ae106ff421b2400e38b97bd32c1f
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-18 00:04:41 +0000

    Sync LeetCode submission Runtime - 3 ms (98.30%), Memory - 18.1 MB (86.18%)

--- submissions/0038-count-and-say/solution.py
+++ submissions/0038-count-and-say/solution.py
@@ -2,19 +2,19 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         curr = '1'
         for i in range(n - 1):
-            nxt = ''
+            nxt = []
             cr = curr[0]
             count = 1
             for c in curr[1:]:
                 if c == cr:
                     count += 1
                 else:
-                    nxt += str(count) + cr
+                    nxt.append(str(count) + cr)
                     cr = c
                     count = 1
 
-            nxt += str(count) + cr
-            curr = nxt
+            nxt.append(str(count) + cr)
+            curr = "".join(nxt)
 
         return curr
         
