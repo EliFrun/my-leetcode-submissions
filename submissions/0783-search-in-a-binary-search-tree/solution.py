6106c3e8ee74f2daa9093b230d89dae490e216bb
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-21 19:38:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (84.21%)

---
+++ submissions/0783-search-in-a-binary-search-tree/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def solve(node):
+            if not node:
+                return None
+            if val < node.val:
+                return solve(node.left)
+            if val > node.val:
+                return solve(node.right)
+            return node
+        return solve(root)
+        
