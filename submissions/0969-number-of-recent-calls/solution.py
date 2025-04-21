45cdc4e918b42a7c522d1052fb204710b6c87028
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-21 16:29:03 +0000

    Sync LeetCode submission Runtime - 88 ms (18.25%), Memory - 23.3 MB (91.48%)

--- submissions/0969-number-of-recent-calls/solution.py
+++ submissions/0969-number-of-recent-calls/solution.py
@@ -7,8 +7,3 @@ class RecentCounter:
     def ping(self, t: int) -> int:
         self.lis.add(t)
         return self.lis.bisect_right(t) - self.lis.bisect_left(t - 3000)
-
-
-# Your RecentCounter object will be instantiated and called as such:
-# obj = RecentCounter()
-# param_1 = obj.ping(t)
