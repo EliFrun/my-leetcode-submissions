e73a981ae71ef00b8bb0bfc95a3ea944e0e7ae9f
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-25 13:21:15 +0000

    Sync LeetCode submission Runtime - 40 ms (72.83%), Memory - 17.6 MB (83.42%)

--- submissions/1441-minimum-flips-to-make-a-or-b-equal-to-c/solution.py
+++ submissions/1441-minimum-flips-to-make-a-or-b-equal-to-c/solution.py
@@ -1,23 +1,18 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
-        a = bin(a)[2:]
-        b = bin(b)[2:]
-        c = bin(c)[2:]
-        
-        
-        foo = max(len(a), len(b), len(c))
-        a = '0' * (foo - len(a)) + a
-        b = '0' * (foo - len(b)) + b
-        c = '0' * (foo - len(c)) + c
-        
+        a, b, c = bin(a)[2:], bin(b)[2:], bin(c)[2:]
+        a = '0' * (max(len(a), len(b), len(c)) - len(a)) + a
+        b = '0' * (max(len(a), len(b), len(c)) - len(b)) + b
+        c = '0' * (max(len(a), len(b), len(c)) - len(c)) + c
         ret = 0
-        for i, j, k in zip(a, b, c):
-            if k == "1":
-                ret += 0 if i == "1" or j == "1" else 1
-            if k == '0':
-                if i == "1" and j == "1":
-                    ret += 2
-                elif not (i == "0" and j == "0"):
+        for x,y,z in zip(a,b,c):
+            x,y,z = int(x), int(y), int(z)
+            if z == 1:
+                if x == 0 and y == 0:
                     ret += 1
-        
+            else:
+                ret += x + y
+
         return ret
+                    
+        
