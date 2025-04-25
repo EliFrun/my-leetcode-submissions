ec953ed5c714b16819a1609a3878bd0ed8c6763d
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-25 13:13:01 +0000

    Sync LeetCode submission Runtime - 7 ms (18.39%), Memory - 20.7 MB (15.65%)

---
+++ submissions/0747-min-cost-climbing-stairs/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        @cache
+        def solve(i):
+            if i >= len(cost):
+                return 0
+            
+            return cost[i] + min(solve(i + 1), solve(i + 2))
+
+        return min(solve(0), solve(1))
+            
+        
