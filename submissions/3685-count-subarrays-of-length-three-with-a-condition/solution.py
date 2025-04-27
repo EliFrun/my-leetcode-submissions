4785d334db340c9f1f0ed107c66031fdfd8f50d6
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-04-27 00:36:14 +0000

    Sync LeetCode submission Runtime - 11 ms (25.74%), Memory - 17.8 MB (82.28%)

---
+++ submissions/3685-count-subarrays-of-length-three-with-a-condition/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        return sum(1 if nums[i-1] + nums[i + 1] == nums[i] / 2 else 0  for i in range(1, len(nums) - 1))
+        
