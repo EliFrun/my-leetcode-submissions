0a2d5142a791841990be91dd3dea0a517e3ab3ac
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-05-04 00:06:33 +0000

    Sync LeetCode submission Runtime - 12 ms (34.75%), Memory - 25.9 MB (77.63%)

---
+++ submissions/1227-number-of-equivalent-domino-pairs/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        return sum(v * (v - 1) // 2 for v in Counter([tuple(sorted(x)) for x in dominoes]).values())
+        
