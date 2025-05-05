af3b6f5cf694746b5e21a240842673a1f70822f6
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-05-05 00:21:23 +0000

    Sync LeetCode submission Runtime - 11 ms (12.74%), Memory - 20.5 MB (12.68%)

--- submissions/0806-domino-and-tromino-tiling/solution.py
+++ submissions/0806-domino-and-tromino-tiling/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def numTilings(self, m: int) -> int:
-        mod = 10 ** 9 + 7
-        @functools.cache
-        def solve(n):
-            nonlocal mod
-            if n == 1:
-                return 1
-            if n == 2:
-                return 2
-            if n == 3:
-                return 5
-            if n == 4:
-                return 11
-            return ((2 * solve(n - 1)) % mod + solve(n - 3) % mod) % mod
+        @cache
+        def solve(full, left):
+            if left <= 0:
+                return 0
+            if left == 1:
+                return 1 if full else 0
+            if left == 2:
+                return 2 if full else 1
+            if full:
+                return 2 * solve(False, left - 1) + solve(True, left - 1) +  solve(True, left - 2)
+            return solve(False, left - 1) + solve(True, left - 2)
 
-        return solve(m)
+        return solve(True, m) % 1_000_000_007
+
         
