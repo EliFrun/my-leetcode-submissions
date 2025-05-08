6c8679ac266b44176d133afb405ee561870f2e14
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-05-08 00:20:09 +0000

    Sync LeetCode submission Runtime - 2545 ms (32.25%), Memory - 100.9 MB (48.64%)

---
+++ submissions/3628-find-minimum-time-to-reach-last-room-ii/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        h = [(0,0,0)]
+        dirs = [0,1,0,-1,0]
+        while h:
+            t, x, y = heappop(h)
+            if x == len(moveTime) - 1 and y == len(moveTime[0]) - 1:
+                return t
+            if moveTime[x][y] < 0:
+                continue
+            moveTime[x][y] = -1
+            for i in range(4):
+                nx, ny = x + dirs[i], y + dirs[i + 1]
+                if nx < 0 or ny < 0 or nx >= len(moveTime) or ny >= len(moveTime[0]):
+                    continue
+                if moveTime[nx][ny] >= 0:
+                    heappush(h, (max(moveTime[nx][ny], t) + (1 if (x + y) % 2 == 0 else 2), nx, ny))
+
+
+        return -1
+        
