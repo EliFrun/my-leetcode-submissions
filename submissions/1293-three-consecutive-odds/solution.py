15270fe69123298232dcea874c1e9a30e0f1c5aa
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-05-11 00:22:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (84.94%)

--- submissions/1293-three-consecutive-odds/solution.py
+++ submissions/1293-three-consecutive-odds/solution.py
@@ -1,8 +1,11 @@
+
+
+
+
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
         for i in range(len(arr) - 2):
             if arr[i] & arr[i + 1] & arr[i + 2] & 1 == 1:
                 return True
 
-        return False
-        
+        return False 
