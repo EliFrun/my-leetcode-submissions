3c2e02ced3f0cbd082b7137a6dce78f549d1d4b5
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-05-22 19:41:19 +0000

    Sync LeetCode submission Runtime - 3 ms (10.14%), Memory - 17.7 MB (81.08%)

--- submissions/1401-number-of-burgers-with-no-waste-of-ingredients/solution.py
+++ submissions/1401-number-of-burgers-with-no-waste-of-ingredients/solution.py
@@ -1,10 +1,12 @@
 class Solution:
     def numOfBurgers(self, tomatoSlices: int, cheeseSlices: int) -> List[int]:
-        s = (4 * cheeseSlices - tomatoSlices) / 2
-        j = cheeseSlices - s
-        if int(s) != s or int(j) != j:
+        s = cheeseSlices
+        j = (tomatoSlices - 2 * s) / 2
+        if int(j) != j:
             return []
-        if s < 0 or j < 0:
+        if s - j < 0 or j < 0:
             return []
-        return [int(j), int(s)]
+        
+        return [int(j), int(s - j)]
+
         
