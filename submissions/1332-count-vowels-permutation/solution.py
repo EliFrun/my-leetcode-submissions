3f4572dbc8cf940a854dfa7e2d5f22952952121c
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-05-29 01:15:49 +0000

    Sync LeetCode submission Runtime - 206 ms (70.60%), Memory - 22.8 MB (46.26%)

---
+++ submissions/1332-count-vowels-permutation/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        dp = [[0] * 5 for _ in range(n)]
+        dp[0] = [1] * 5
+        for i in range(1, n):
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % 1_000_000_007
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 1_000_000_007
+            dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % 1_000_000_007
+            dp[i][3] = (dp[i - 1][2]) % 1_000_000_007
+            dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % 1_000_000_007
+
+        return sum(dp[-1]) % 1_000_000_007        
