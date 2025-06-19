0e8d7f57851fb270660e77f7c01cb8730e5e8d89
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-06-19 16:12:53 +0000

    Sync LeetCode submission Runtime - 7 ms (66.63%), Memory - 21.2 MB (88.12%)

--- submissions/0378-kth-smallest-element-in-a-sorted-matrix/solution.py
+++ submissions/0378-kth-smallest-element-in-a-sorted-matrix/solution.py
@@ -1,10 +1,14 @@
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:  
-        lis = []
-        for x in matrix:
-            lis += x
-            
-        lis.sort()
-        
-        return lis[k - 1]
-        
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        def cnt(v):
+            return sum([bisect_right(matrix[i], v) for i in range(len(matrix))])
+        lower, upper = matrix[0][0], matrix[-1][-1]
+
+        while lower < upper:
+            middle = (lower + upper) // 2
+            if cnt(middle) < k:
+                lower = middle + 1
+            else:
+                upper = middle
+
+        return lower        
