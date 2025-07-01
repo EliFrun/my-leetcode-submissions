abcf23adbc03d6fe57f0cd0da954eba8db87a24f
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-07-01 17:37:55 +0000

    Sync LeetCode submission Runtime - 1360 ms (23.96%), Memory - 18.6 MB (83.85%)

--- submissions/2059-unique-length-3-palindromic-subsequences/solution.py
+++ submissions/2059-unique-length-3-palindromic-subsequences/solution.py
@@ -1,21 +1,17 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        left = set()
-        right_c = Counter(s)
-        right = set(list(s))
+        left = defaultdict(int)
+        right = Counter(s)
 
         chars = defaultdict(set)
         for c in s:
-            right_c[c] -= 1
-            if right_c[c] <= 0:
-                right.remove(c)
-            chars[c] = chars[c].union(left.intersection(right))
-            left.add(c)
+            right[c] -= 1
+            if right[c] == 0:
+                right.pop(c)
+            
+            chars[c] |= set(right.keys()) & set(left.keys())
+            left[c] += 1
 
-        ret = 0
-        for c, opts in chars.items():
-            ret += len(opts)
-
-        return ret
+        return sum([len(x) for x in chars.values()])
 
         
