ee3acc50a9b63c740229f4b550f9a2bec833997b
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-07-06 01:45:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (83.90%)

---
+++ submissions/3484-lexicographically-smallest-string-after-a-swap/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSmallestString(self, s: str) -> str:
+        s = list(s)
+        for i in range(len(s) - 1):
+            if int(s[i]) & 1 == int(s[i + 1]) & 1 and int(s[i]) > int(s[i + 1]):
+                s[i], s[i + 1] = s[i + 1], s[i]
+                break
+
+        return ''.join(s)
+
+        
