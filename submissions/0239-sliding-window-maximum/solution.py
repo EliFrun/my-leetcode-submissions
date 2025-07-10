e455379a5996a91042a3e469c8f8341c01c0d605
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-07-10 14:13:26 +0000

    Sync LeetCode submission Runtime - 967 ms (5.01%), Memory - 31.1 MB (100.00%)

--- submissions/0239-sliding-window-maximum/solution.py
+++ submissions/0239-sliding-window-maximum/solution.py
@@ -1,18 +1,13 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        current_window = [
-            i for i,v in sorted(
-                list(enumerate(nums[:k])), reverse=True, key=lambda x: (x[1], -x[0]))
-        ]
+        s = SortedList(nums[:k])
         
-        ret = [nums[current_window[0]]]
+        ret = [s[-1]]
         for i in range(k, len(nums)):
-            while current_window and current_window[0] <= i - k:
-                current_window.pop(0)
-            while current_window and nums[current_window[-1]] < nums[i]:
-                current_window.pop()
-            current_window.append(i)
-            ret.append(nums[current_window[0]])
+            s.add(nums[i])
+            s.remove(nums[i - k])
+            ret.append(s[-1])
+           
         return ret
             
 
