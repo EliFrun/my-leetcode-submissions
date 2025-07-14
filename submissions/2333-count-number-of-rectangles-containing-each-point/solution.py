71c566808d6fe2bb60b9b49c74bbb5d598a0b2f4
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-07-14 01:13:40 +0000

    Sync LeetCode submission Runtime - 1674 ms (27.81%), Memory - 36.1 MB (84.11%)

--- submissions/2333-count-number-of-rectangles-containing-each-point/solution.py
+++ submissions/2333-count-number-of-rectangles-containing-each-point/solution.py
@@ -1,16 +1,17 @@
 class Solution:
     def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
-        r = defaultdict(SortedList)
+        rects = defaultdict(SortedList)
         for x,y in rectangles:
-            r[y - 1].add(x)
-
-        keys = SortedList(r.keys())
+            rects[y].add(x)
 
         ret = []
         for x,y in points:
-            cnt = 0
-            for i in keys[keys.bisect_left(y - 1):]:
-                cnt += len(r[i]) - r[i].bisect_left(x)
+            v = 0
+            for key in [k for k in rects.keys() if k >= y]:
+                v += len(rects[key]) - rects[key].bisect_right(x - 1)
 
-            ret.append(cnt)
+            ret.append(v)
         return ret
+
+
+        
