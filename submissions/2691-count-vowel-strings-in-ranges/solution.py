94cb9c23064ac69b6cd86d7f858b2be1319feb6c
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-08-01 01:18:20 +0000

    Sync LeetCode submission Runtime - 15 ms (95.66%), Memory - 49.5 MB (13.75%)

--- submissions/2691-count-vowel-strings-in-ranges/solution.py
+++ submissions/2691-count-vowel-strings-in-ranges/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        prefix_sum = [0]
-        for s in words:
-            if s[0] in 'aeiou' and s[-1] in 'aeiou':
-                prefix_sum.append(prefix_sum[-1] + 1)
-            else:
-                prefix_sum.append(prefix_sum[-1])
-        
-        prefix_sum = prefix_sum
-        return [prefix_sum[j + 1] - prefix_sum[i] for i,j in queries]
+        prefix = [0]
+        for word in words:
+            v = prefix[-1]
+            if word[0] in 'aeiou' and word[-1] in 'aeiou':
+                v += 1
+            prefix.append(v)
+
+        return [prefix[r + 1] - prefix[l] for l, r in queries]
         
