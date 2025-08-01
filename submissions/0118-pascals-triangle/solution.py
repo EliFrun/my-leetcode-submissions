e4279efb9092792ad732a5019e23672cfbf17ad3
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-08-01 01:10:25 +0000

    Sync LeetCode submission Runtime - 4 ms (3.71%), Memory - 17.9 MB (83.27%)

--- submissions/0118-pascals-triangle/solution.py
+++ submissions/0118-pascals-triangle/solution.py
@@ -1,20 +1,14 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0:
-            return []
-        if numRows == 1:
-            return [[1]]
-        if numRows == 2:
-            return [[1], [1, 1]]
-        ret = [[1], [1,1]]
-        for i in range(2, numRows):
-            lis = [0] * (i + 1)
-            lis[0] = 1
-            lis[-1] = 1
-            
-            for j in range(1, i):
-                lis[j] = ret[i - 1][j - 1] + ret[i - 1][j]
-                
-            ret.append(lis)
-            
+    def generate(self, r: int) -> List[List[int]]:
+        ret = [[1]]
+        curr = [1] * min(r, 2)
+        for _ in range(r - 1):
+            ret.append(curr)
+            nxt = [1]
+            for i in range(len(curr) - 1):
+                nxt.append(curr[i] + curr[i + 1])
+            nxt.append(1)
+            curr = nxt
+
         return ret
+        
