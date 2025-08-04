4355ea1f4f64adfba6527949001154dfbb21d0ed
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-08-04 21:18:26 +0000

    Sync LeetCode submission Runtime - 191 ms (20.89%), Memory - 19.2 MB (88.54%)

--- submissions/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solution.py
+++ submissions/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solution.py
@@ -1,39 +1,20 @@
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
-        costs = [[ -1 for _ in range(len(grid[0]))] for __ in range(len(grid))]
-        curr = set([(0,0)])
-        layer = 0
+        costs = [[float('inf') for _ in range(len(grid[0]))] for __ in range(len(grid))]
+        curr = [(0,0,0)]
         while curr:
-            nxt = set()
-            for i,j in curr:
-                curr_idx = (i, j)
-                while curr_idx:
-                    i, j = curr_idx
-                    costs[i][j] = layer
-                    if i == len(grid) - 1 and j == len(grid[0]) - 1:
-                        return layer
-                    lis = [(0, 1), (0, -1), (1,0), (-1,0)]
-                    di, dj = lis[grid[i][j] - 1]
-                    lis.pop(grid[i][j] - 1)
-                    for dbi, dbj in lis:
-                        if (i + dbi >= 0 and
-                            i + dbi < len(grid) and 
-                            j + dbj >= 0 and
-                            j + dbj < len(grid[0]) and
-                            costs[i + dbi][j + dbj] == -1
-                        ):
-                            nxt.add((i + dbi, j + dbj))
-                    if (i + di >= 0 and
-                        i + di < len(grid) and 
-                        j + dj >= 0 and
-                        j + dj < len(grid[0]) and
-                        costs[i + di][j + dj] == -1
-                    ):
-                        curr_idx = (i + di, j + dj)
-                    else:
-                        curr_idx = None
-
-            curr = nxt
-            layer += 1
+            cost, x, y = heappop(curr)
+            if cost >= costs[x][y]:
+                continue
+            if x == len(grid) - 1 and y == len(grid[0]) - 1:
+                return cost
+            costs[x][y] = cost
+            for i, (dx, dy) in enumerate([(0, 1), (0, -1), (1,0), (-1,0)]):
+                dcost = 0 if i == grid[x][y] - 1 else 1
+                if x + dx < 0 or x + dx >= len(grid) or y + dy < 0 or y + dy >= len(grid[0]):
+                    continue
+                if cost + dcost >= costs[x + dx][y + dy]:
+                    continue
+                heappush(curr, (cost + dcost, x + dx, y + dy))
 
         return 0
