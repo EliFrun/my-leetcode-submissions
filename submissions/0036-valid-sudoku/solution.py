9bbf95385a7e6475b205d0d456be29b5e5bf5f5c
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-08-30 03:50:05 +0000

    Sync LeetCode submission Runtime - 3 ms (77.71%), Memory - 17.7 MB (82.74%)

--- submissions/0036-valid-sudoku/solution.py
+++ submissions/0036-valid-sudoku/solution.py
@@ -1,38 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in board:
-            d = set()
-            for i in row:
-                if i != '.':
-                    if i in d:
-                        return False
-                    else:
-                        d.add(i)
-            
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        boxes = defaultdict(set)
         for i in range(9):
-            d = set()
             for j in range(9):
-                if board[j][i] != '.':
-                    if board[j][i] in d:
-                        return False
-                    else:
-                        d.add(board[j][i])
+                if board[i][j] == '.':
+                    continue
+                if board[i][j] in rows[i]:
+                    return False
+                rows[i].add(board[i][j])
+                if board[i][j] in columns[j]:
+                    return False
+                columns[j].add(board[i][j])
+                if board[i][j] in boxes[(i//3, j//3)]:
+                    return False
+                boxes[(i//3,j//3)].add(board[i][j])
 
-            
-        for i in range(3):
-            for j in range(3):
-                d = set()
-                for k in range(3):
-                    for l in range(3):
-                        if board[3 * i + k][3 * j + l] != '.':
-                            if board[3 * i + k][3 * j + l] in d:
-                                return False
-                            else:
-                                d.add(board[3 * i + k][3 * j + l])
-
-                
         return True
-            
-                    
-
         
