f78ba2a4715104f9241da2ac43a7e06a0fc2c478
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-09-15 00:01:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (85.21%)

--- submissions/1264-maximum-number-of-words-you-can-type/solution.py
+++ submissions/1264-maximum-number-of-words-you-can-type/solution.py
@@ -1,11 +1,4 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        s = set(brokenLetters)
-        text = text.split(" ")
-        ret = 0
-        for t in text:
-            if not any(x in s for x in t):
-                ret += 1
-                
-        return ret
+        return len([x for x in text.split(' ') if x and not any(y in x for y in brokenLetters)])
         
