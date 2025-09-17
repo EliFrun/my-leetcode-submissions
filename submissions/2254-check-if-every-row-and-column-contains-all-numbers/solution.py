ef4966ad73328425adec88d1dfb7448da3ae19c9
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-09-17 14:24:48 +0000

    Sync LeetCode submission Runtime - 48 ms (27.23%), Memory - 18.7 MB (86.93%)

---
+++ submissions/2254-check-if-every-row-and-column-contains-all-numbers/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        n = len(matrix)
+        for i in range(n):
+            s = set()
+            for j in range(n):
+                if matrix[i][j] in s:
+                    return False
+                if matrix[i][j] > n:
+                    return False
+                s.add(matrix[i][j])
+
+        for j in range(n):
+            s = set()
+            for i in range(n):
+                if matrix[i][j] in s:
+                    return False
+                if matrix[i][j] > n:
+                    return False
+                s.add(matrix[i][j])
+
+
+        return True
