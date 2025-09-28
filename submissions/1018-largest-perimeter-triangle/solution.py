84e005cb0e67d59ed1ed6aa543474d8b28c9cd30
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-09-28 00:22:55 +0000

    Sync LeetCode submission Runtime - 12 ms (75.38%), Memory - 18.9 MB (85.55%)

--- submissions/1018-largest-perimeter-triangle/solution.py
+++ submissions/1018-largest-perimeter-triangle/solution.py
@@ -1,9 +1,8 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        nums.sort(reverse=True)
-        for i in range(len(nums) - 2):
-            if nums[i] < nums[i + 1] + nums[i + 2]:
-                return sum(nums[i:i + 3])
-            
-        return 0
+        nums.sort()
+        for i in range(len(nums) - 1, 1, -1):
+            if nums[i] < nums[i - 1] + nums[i - 2]:
+                return nums[i] + nums[i - 1] + nums[i - 2]
         
+        return 0
