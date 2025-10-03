7c70a715bec02a16f9eabbf43a199d3c2baab535
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-10-03 00:54:22 +0000

    Sync LeetCode submission Runtime - 223 ms (8.89%), Memory - 24.2 MB (21.48%)

--- submissions/0407-trapping-rain-water-ii/solution.py
+++ submissions/0407-trapping-rain-water-ii/solution.py
@@ -1,52 +1,37 @@
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
-        m = len(heightMap[0])
-        n = len(heightMap)
-        h = []
-        visited = set()
-        for i in range(1, m - 1):
-            h.append((heightMap[0][i], (0, i)))
-            visited.add((0,i))
-
-        for i in range(1, m - 1):
-            h.append((heightMap[n - 1][i], (n - 1, i)))
-            visited.add((n - 1,i))
-
-        for i in range(1, n - 1):
-            h.append((heightMap[i][0], (i, 0)))
-            visited.add((i, 0))
-
-        for i in range(1, n - 1):
-            h.append((heightMap[i][m - 1], (i, m - 1)))
-            visited.add((i, m - 1))
-
-        visited.add((0,0))
-        visited.add((0, m - 1))
-        visited.add((n - 1,0))
-        visited.add((n - 1, m - 1))
-
-        heapq.heapify(h)
         ret = 0
-        fill = [[0] * m for _ in range(n)]
-        while len(visited) < m * n:
-            height, (x, y) = heapq.heappop(h)
+        q = []
+        for i in range(1, len(heightMap) - 1):
+            heappush(q, (heightMap[i][0], i, 0))
+            heappush(q, (heightMap[i][len(heightMap[0]) - 1], i, len(heightMap[0]) - 1))
+
+        for j in range(1, len(heightMap[0]) - 1):
+            heappush(q, (heightMap[0][j], 0, j))
+            heappush(q, (heightMap[len(heightMap) - 1][j], len(heightMap) - 1, j))
+
+        visited = set([(x,y) for h, x, y in q])
+        visited.update([
+            (0,0),
+            (0, len(heightMap[0]) - 1),
+            (len(heightMap) - 1, 0),
+            (len(heightMap) - 1, len(heightMap[0]) - 1)
+        ])
+        while q:
+            h, x, y = heappop(q)
+            visited.add((x,y))
             dirs = [0,1,0,-1,0]
             for i in range(4):
-                dx = dirs[i]
-                dy = dirs[i + 1]
-                if x + dx < 0 or x + dx >= n:
+                dx, dy = dirs[i], dirs[i + 1]
+                if not (0 <= x + dx < len(heightMap) and 0 <= y + dy < len(heightMap[0])):
                     continue
-                if y + dy < 0 or y + dy >= m:
-                    continue
-
                 if (x + dx, y + dy) in visited:
                     continue
-
-                fill[x + dx][y + dy] = max(0, height - heightMap[x + dx][y + dy])
-                ret += fill[x + dx][y + dy]
                 visited.add((x + dx, y + dy))
-                heapq.heappush(h, (max(heightMap[x + dx][y + dy], height), (x + dx, y + dy)))
+                ret += max(0, h - heightMap[x + dx][y + dy])
+                heightMap[x + dx][y + dy] = max(h, heightMap[x + dx][y + dy])
+                heappush(q, (heightMap[x + dx][y + dy], x + dx, y + dy))
 
-        #print(*fill, sep='\n')
+        print(*heightMap, sep='\n')
         return ret
-            
+
