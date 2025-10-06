efacf2f6aeedc3be5f6babe5c2cf2f03589b5610
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-10-06 02:53:46 +0000

    Sync LeetCode submission Runtime - 132 ms (10.91%), Memory - 20.2 MB (80.43%)

--- submissions/2038-nearest-exit-from-entrance-in-maze/solution.py
+++ submissions/2038-nearest-exit-from-entrance-in-maze/solution.py
@@ -1,37 +1,34 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        curr = set()
+        entrance = tuple(entrance)
+        curr = set([entrance])
+
         dirs = [0,1,0,-1,0]
-        for i in range(4):
-            dx = dirs[i]
-            dy = dirs[i + 1]
-            if entrance[0] + dx < 0 or entrance[0] + dx > len(maze) - 1:
-                continue
-            if entrance[1] + dy < 0 or entrance[1] + dy > len(maze[0]) - 1:
-                continue
-            if maze[entrance[0] + dx][entrance[1] + dy] == '+':
-                continue
-            curr.add((entrance[0] + dx, entrance[1] + dy))
-        visited = set([tuple(entrance)])
-        layer = 0
+        visited = set()
+        ret = -1
         while curr:
-            layer += 1
+            ret += 1
             nxt = set()
             for x,y in curr:
-                visited.add((x,y))
-                dirs = [0,1,0,-1,0]
+                if (x,y) != entrance and (x == 0 or y == 0):
+                    return ret
+                if (x,y) != entrance and (x == len(maze) - 1 or y == len(maze[0]) - 1):
+                    return ret
+
                 for i in range(4):
-                    dx = dirs[i]
-                    dy = dirs[i + 1]
-                    if x + dx < 0 or x + dx > len(maze) - 1:
-                        return layer
-                    if y + dy < 0 or y + dy > len(maze[0]) - 1:
-                        return layer
-                    if maze[x + dx][y + dy] != "+":
-                        nxt.add((x + dx, y + dy))
-            curr = nxt - visited
-
+                    dx, dy = dirs[i], dirs[i + 1]
+                    if not 0 <= x + dx < len(maze):
+                        continue
+                    if not 0 <= y + dy < len(maze[0]):
+                        continue
+                    
+                    if maze[x + dx][y + dy] == '+':
+                        continue
+                    
+                    if (x + dx, y + dy) in visited:
+                        continue
+                    
+                    visited.add((x + dx, y + dy))
+                    nxt.add((x + dx, y + dy))
+            curr = nxt
         return -1
-
-    
-        
