79e72767dc7afdf731d210490de6b7c93ecf2919
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-10-20 00:03:15 +0000

    Sync LeetCode submission Runtime - 3 ms (16.84%), Memory - 18 MB (82.12%)

---
+++ submissions/2137-final-value-of-variable-after-performing-operations/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        return len([x for x in operations if '++' in x]) - len([x for x in operations if '--' in x])
+        
