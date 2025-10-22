600f029ecc6f20109cb92560f1b2d0ae23c59145
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-10-22 01:57:59 +0000

    Sync LeetCode submission Runtime - 4268 ms (5.49%), Memory - 33.1 MB (99.45%)

--- submissions/3640-maximum-frequency-of-an-element-after-performing-operations-ii/solution.py
+++ submissions/3640-maximum-frequency-of-an-element-after-performing-operations-ii/solution.py
@@ -1,31 +1,12 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
-        s = SortedList(nums)
-        cands = set()
-        for i in s:
-            cands.update([i - k, i, i + k])
-        
-        s.add(-10 ** 10)
-        s.add(10 ** 10)
+        nums = SortedList(nums)
 
         ret = 0
-        i = 0
-        for num in cands:
-            #print(num)
-            left = s.bisect_left(num) - s.bisect_left(num - k)
-            middle = s.bisect_right(num) - s.bisect_left(num)
-            right = s.bisect_right(num + k) - s.bisect_right(num)
-            #print(left, middle, right)
-            cand = left + right
-            cand = min(cand, numOperations)
-            ret = max(ret, cand + middle)
-            if i == len(s) - 1:
-                break
-            if num + k < s[i + 1]:
-                num += k
-            else:
-                i += 1
-                num = s[i]
-
+        for num in nums:
+            for v in [num - k, num, num + k]:
+                c = nums.bisect_right(v) - nums.bisect_left(v)
+                width = nums.bisect_right(v + k) - nums.bisect_left(v - k) - c
+                ret = max(ret, c + min(numOperations, width))
         return ret
         
