745b1643ba679123059ae8c332b6e22fd54fe7ca
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-10-23 14:36:23 +0000

    Sync LeetCode submission Runtime - 967 ms (8.16%), Memory - 31.2 MB (95.59%)

--- submissions/0220-contains-duplicate-iii/solution.py
+++ submissions/0220-contains-duplicate-iii/solution.py
@@ -1,25 +1,17 @@
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
-        a = sorted(list(enumerate(nums)), key=lambda x: x[1])
-
-        curr = [a[0]]
-        indexes = SortedList([a[0][0]])
-        i = 1
-        while i < len(a):
-            if a[i][1] > curr[0][1] + valueDiff:
-                while curr and curr[0][1] + valueDiff < a[i][1]:
-                    idx, v = curr.pop(0)
-                    indexes.remove(idx)
-            
-            idx = indexes.bisect_left(a[i][0])
-            comps = [max(idx - 1, 0), min(idx, len(indexes) - 1), min(idx + 1, len(indexes) - 1)]
-            for comp in comps:
-                if indexes and abs(indexes[comp] - a[i][0]) <= indexDiff:
+        l = SortedList()
+        for i,v in enumerate(nums):
+            if l:
+                idx = l.bisect_left(v)
+                if idx - 1 >= 0 and abs(l[idx - 1] - v) <= valueDiff:
                     return True
-            curr.append(a[i])
-            indexes.add(a[i][0])
-            i += 1
-
+                if idx < len(l) and abs(l[idx] - v) <= valueDiff:
+                    return True
+                if idx + 1 < len(l) and abs(l[idx + 1] - v) <= valueDiff:
+                    return True
+            l.add(v)
+            if i >= indexDiff: (l.remove(nums[i - indexDiff]))
         return False
 
 
