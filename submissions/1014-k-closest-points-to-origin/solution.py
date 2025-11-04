9c6bbdc272808895be4521bf0b26e968f0215f69
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-04 19:05:21 +0000

    Sync LeetCode submission Runtime - 35 ms (96.34%), Memory - 21.8 MB (99.13%)

--- submissions/1014-k-closest-points-to-origin/solution.py
+++ submissions/1014-k-closest-points-to-origin/solution.py
@@ -1,3 +1,4 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return sorted(points, key=lambda x: x[0] * x[0] + x[1] * x[1])[:k]
+        return sorted(points, key=lambda x: sqrt(x[0] ** 2 + x[1] ** 2))[:k]
+        
