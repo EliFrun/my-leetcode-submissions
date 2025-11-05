ecadd62a23e095a58bc5622f682023e53f66e7ff
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-05 15:48:55 +0000

    Sync LeetCode submission Runtime - 7 ms (31.20%), Memory - 17.8 MB (83.06%)

--- submissions/0069-sqrtx/solution.py
+++ submissions/0069-sqrtx/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x <= 1:
-            return x
-        curr, prev = 0.5 * (x + 1), x
-        steps = 0
-        while abs(prev - curr) > 0.1:
-            steps += 1
-            nxt = 0.5 * (curr + x/curr)
-            prev = curr
-            curr = nxt
-
-        return int(curr)
-
+        ret = 0
+        left, right = 0, x
+        while left <= right:
+            middle = (left + right) // 2
+            if middle * middle <= x:
+                ret = middle
+                left = middle + 1
+            else:
+                right = middle - 1
+        return ret
+        
