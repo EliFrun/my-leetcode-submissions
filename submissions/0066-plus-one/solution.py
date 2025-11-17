e7d2f7ea0d5b332d7933a67bf3dffeb939a18cd6
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 15:31:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (47.30%)

--- submissions/0066-plus-one/solution.py
+++ submissions/0066-plus-one/solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-            return list(str(int(''.join([str(s) for s in digits])) + 1))
+        carry = 1
+        for i in range(len(digits) - 1, -1 ,-1):
+            next_carry = (digits[i] + carry) // 10
+            digits[i] = (digits[i] + carry) % 10
+            carry = next_carry
+        if carry:
+            return [1] + digits
+        else:
+            return digits
+            
+        
