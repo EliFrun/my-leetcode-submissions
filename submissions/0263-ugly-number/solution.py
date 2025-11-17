b38621108c4c5c182db6a59bdaed6d0e3d655a08
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 19:09:23 +0000

    Sync LeetCode submission Runtime - 1 ms (30.27%), Memory - 18 MB (81.09%)

--- submissions/0263-ugly-number/solution.py
+++ submissions/0263-ugly-number/solution.py
@@ -2,15 +2,9 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         if n == 0:
             return False
-
-        while n % 2 == 0:
-            n //= 2
-            
-        while n % 3 == 0:
-            n //= 3
-            
-        while n % 5 == 0:
-            n //= 5
-            
+        for v in [2,3,5]:
+            while n % v == 0:
+                n //= v
         return n == 1
+
         
