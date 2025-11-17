5e8d35340858c762cf58781736a2cd873bf399c3
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 12:19:58 +0000

    Sync LeetCode submission Runtime - 67 ms (29.57%), Memory - 17.9 MB (82.60%)

--- submissions/1580-shuffle-the-array/solution.py
+++ submissions/1580-shuffle-the-array/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        return [nums[i // 2] if i % 2 == 0 else nums[n + i//2] for i in range(len(nums))]
+        return [nums[i//2] if i % 2 == 0 else nums[n + i//2] for i in range(2 * n)]
         
