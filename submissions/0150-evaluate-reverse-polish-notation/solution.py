a8e18a09bedf4832e06da5f303867fe7bece9427
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 12:47:44 +0000

    Sync LeetCode submission Runtime - 3 ms (58.72%), Memory - 19 MB (97.75%)

--- submissions/0150-evaluate-reverse-polish-notation/solution.py
+++ submissions/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,26 +1,17 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-        for i in tokens:
-            if i.isnumeric() or i[1:].isnumeric():
-                stack.append(i)
+        stk = []
+        mp = {
+            "+": lambda a,b: a + b,
+            "-": lambda a,b: b - a,
+            "*": lambda a,b: a * b,
+            "/": lambda a,b: int(b / a),
+        }
+        for token in tokens:
+            if token in "*/+-":
+                stk.append(mp[token](stk.pop(), stk.pop()))
             else:
-                if i == "+":
-                    a = stack.pop()
-                    b = stack.pop()
-                    stack.append(int(a) + int(b))
-                elif i == "-":
-                    a = stack.pop()
-                    b = stack.pop()
-                    stack.append(int(b) - int(a))
-                elif i == "*":
-                    a = stack.pop()
-                    b = stack.pop()
-                    stack.append(int(a) * int(b))
-                elif i == "/":
-                    a = stack.pop()
-                    b = stack.pop()
-                    stack.append(int(int(b) / int(a)))
-                    
-                    
-        return stack[0]
+                stk.append(int(token))
+        return int(stk[0])
+                
+        
