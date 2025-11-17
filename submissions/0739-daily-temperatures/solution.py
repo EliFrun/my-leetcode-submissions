0744125c512a5f440c98d80891379f05b654e874
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 15:25:58 +0000

    Sync LeetCode submission Runtime - 110 ms (38.92%), Memory - 27 MB (89.43%)

--- submissions/0739-daily-temperatures/solution.py
+++ submissions/0739-daily-temperatures/solution.py
@@ -1,17 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        tmp = list(reversed(temperatures))
         stk = []
         ret = []
-        for i in range(len(tmp)):
-            while stk and tmp[i] >= tmp[stk[-1]]:
+        for i in range(len(temperatures) - 1, -1, -1):
+            while stk and temperatures[stk[-1]] <= temperatures[i]:
                 stk.pop()
-            if stk:
-                ret.append(i - stk[-1])
-            else:
-                ret.append(0)
+            idx = stk[-1] if stk else i
+            ret.append(idx - i)
             stk.append(i)
-
         return ret[::-1]
-
         
