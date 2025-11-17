0d1abe80971e571683833ccf218bc69bf4111b2a
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 12:25:07 +0000

    Sync LeetCode submission Runtime - 14 ms (52.57%), Memory - 19 MB (83.58%)

--- submissions/0645-set-mismatch/solution.py
+++ submissions/0645-set-mismatch/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        l = [False] * len(nums)
-        for v in nums:
-            l[v - 1] = True
-
+        ret = [-1,-1]
         c = Counter(nums)
-        return (
-            [k for k,v in c.items() if v > 1][0],
-            [i + 1 for i,v in enumerate(l) if not v][0]
-        )
-
+        for i in range(1, len(nums) + 1):
+            if i not in c:
+                ret[1] = i
+            if c[i] == 2:
+                ret[0] = i
+        return ret
         
