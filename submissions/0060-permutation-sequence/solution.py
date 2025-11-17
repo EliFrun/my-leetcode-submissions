d3bf2250deb5890c711b6c31c3dbe0e166774cc0
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 19:47:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (84.49%)

--- submissions/0060-permutation-sequence/solution.py
+++ submissions/0060-permutation-sequence/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        k -= 1
-        vals = [str(i) for i in range(1, n + 1)]
-        ret = ""
-        while n > 0:
-            i = k // factorial(n - 1)
-            ret += vals[i]
-            vals.pop(i)
-            k %= factorial(n - 1)
-            n -= 1
+        vals = list(range(1, n + 1))
+        ret = ''
+        while vals:
+            if k == 0:
+                ret += ''.join([str(x) for x in vals])
+                break
+            idx = (k - 1) // factorial(len(vals) - 1)
+            k -= factorial(len(vals) - 1) * idx
+            ret += str(vals.pop(idx))
         return ret
+        
