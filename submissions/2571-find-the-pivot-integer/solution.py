2e54ad2b61b9c29427a61f60199606a3e70bf44d
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-17 12:57:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (78.53%)

--- submissions/2571-find-the-pivot-integer/solution.py
+++ submissions/2571-find-the-pivot-integer/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        val = (n ** 2 + n) / 2
-        val = val ** 0.5
-        if int(val) == val:
-            return int(val)
-        return -1 
+        return int(sqrt((n ** 2 + n)//2)) if sqrt((n ** 2 + n)//2) == int(sqrt((n ** 2 + n)//2)) else -1
+
         
 
         
