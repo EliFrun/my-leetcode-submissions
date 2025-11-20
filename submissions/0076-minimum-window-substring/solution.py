9c88ad7fbbce9043f3e0b21dde792b11eba5a734
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-20 17:03:29 +0000

    Sync LeetCode submission Runtime - 135 ms (26.52%), Memory - 18.4 MB (82.98%)

--- submissions/0076-minimum-window-substring/solution.py
+++ submissions/0076-minimum-window-substring/solution.py
@@ -1,49 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def counter_compare(ref, curr):
-            return all([curr[k] >= v for k,v in ref.items()])
-        t_counter = Counter(t)
-        curr_window = defaultdict(int)
+        c = Counter(t)
+        ret = ''
+        cs = defaultdict(int)
+        below = set(c.keys())
 
-        left, right = 0,0
-        ret_left, ret_right = -1, -1
+        left, right = 0, 0
         while right < len(s):
-            if left == right:
-                curr_window[s[right]] += 1
-                right += 1
-
-            good = counter_compare(t_counter, curr_window)
-            if good:
-                if ret_left == -1:
-                    ret_left = left
-                    ret_right = right
-                elif ret_right - ret_left > right - left:
-                    ret_left = left
-                    ret_right = right
-                
-                curr_window[s[left]] -= 1
-                if curr_window[s[left]] <= 0:
-                    curr_window.pop(s[left])
+            cs[s[right]] += 1
+            if s[right] in below and cs[s[right]] >= c[s[right]]:
+                below.remove(s[right])
+            while not below:
+                if not ret or right - left + 1 < len(ret):
+                    ret = s[left: right + 1]
+                cs[s[left]] -= 1
+                if cs[s[left]] < c[s[left]]:
+                    below.add(s[left])
                 left += 1
+            right += 1
+        
+        return ret
             
-            elif right < len(s):
-                curr_window[s[right]] += 1
-                right += 1
-
-        while left < len(s) and counter_compare(t_counter, curr_window):
-            if ret_left == -1:
-                ret_left = left
-                ret_right = right
-            if right - left < ret_right - ret_left:
-                ret_right = right
-                ret_left = left
-
-            curr_window[s[left]] -= 1
-            if curr_window[s[left]] <= 0:
-                curr_window.pop(s[left])
-            left += 1
-
-        return s[ret_left: ret_right]
             
         
         
