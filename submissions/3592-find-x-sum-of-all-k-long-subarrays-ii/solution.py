0490bcacc36a4c246031186d41ff9a5caa635b8f
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-24 15:20:48 +0000

    Sync LeetCode submission Runtime - 3544 ms (26.42%), Memory - 48.3 MB (23.90%)

--- submissions/3592-find-x-sum-of-all-k-long-subarrays-ii/solution.py
+++ submissions/3592-find-x-sum-of-all-k-long-subarrays-ii/solution.py
@@ -1,36 +1,45 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        c = defaultdict(int)
-        s = SortedList()
-        curr = 0
+        counts = defaultdict(int)
+        lis1 = SortedList()
+        lis2 = SortedList()
         ret = []
-        for i, v in enumerate(nums):
-            if (c[v], v) in s and s.index((c[v], v)) >= len(s) - x:
-                curr -= c[v] * v
-                if len(s) > x:
-                    curr += s[-x - 1][0] * s[-x - 1][1]
-            if (c[v], v) in s:
-                s.remove((c[v], v))
-            c[v] += 1
-            s.add((c[v], v))
-            if s.index((c[v], v)) >= len(s) - x:
-                curr += c[v] * v
-                if len(s) > x:
-                    curr -= s[-x - 1][0] * s[-x - 1][1]
-            if i >= k:
-                v = nums[i - k]
-                if s.index((c[v], v)) >= len(s) - x:
-                    curr -= c[v] * v
-                    if len(s) > x:
-                        curr += s[-x - 1][0] * s[-x - 1][1]
-                s.remove((c[v], v))
-                c[v] -= 1
-                s.add((c[v], v))
-                if s.index((c[v], v)) >= len(s) - x:
-                    curr += c[v] * v
-                    if len(s) > x:
-                        curr -= s[-x - 1][0] * s[-x - 1][1]
-            if i >= k - 1:
-                ret.append(curr)
+        s = 0
+        for i in range(len(nums)):
+            n = nums[i]
+            if (counts[n], n) in lis1:
+                lis1.remove((counts[n], n))
+            elif (counts[n], n) in lis2:
+                s -= counts[n] * n
+                lis2.remove((counts[n], n))
+            counts[n] += 1
+            lis1.add((counts[n], n))
 
+            if i < k - 1:
+                continue
+
+            while lis1 and len(lis2) < x:
+                c1,v1 = lis1.pop()
+                s += c1 * v1
+                lis2.add((c1,v1))
+
+            while lis1 and lis1[-1] > lis2[0]:
+                c1, v1 = lis1.pop(-1)
+                c2, v2 = lis2.pop(0)
+                s -= c2 * v2
+                s += c1 * v1
+                lis1.add((c2, v2))
+                lis2.add((c1, v1))
+
+            ret.append(s)
+            m = nums[i - k + 1]
+            if (counts[m], m) in lis1:
+                lis1.remove((counts[m], m))
+            elif (counts[m], m) in lis2:
+                s -= counts[m] * m
+                lis2.remove((counts[m], m))
+            counts[m] -= 1
+            lis1.add((counts[m], m))
+
+            
         return ret
