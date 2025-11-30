0ff6dd617c4b4231afe3cdd5d6ec6a901f75a75a
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-11-30 00:21:57 +0000

    Sync LeetCode submission Runtime - 71 ms (89.50%), Memory - 38.2 MB (81.91%)

--- submissions/1694-make-sum-divisible-by-p/solution.py
+++ submissions/1694-make-sum-divisible-by-p/solution.py
@@ -1,32 +1,19 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        d = sum(nums) % p
-        if d == 0:
+        s = sum(nums) % p
+        if s == 0:
             return 0
-        
-        cs = [0] * (len(nums) + 1)
-        for i, num in enumerate(nums):
-            cs[i + 1] = num + cs[i]
 
-        m = defaultdict(SortedList)
-        for i, x in enumerate(cs):
-            m[x % p].add(i)
-
+        c = 0
+        d = { 0: -1 }
         ret = float('inf')
-        for k,v in m.items():
-            for i in v:
-                t = (k - d + p) % p
-                if t not in m:
-                    continue
-                idx = min(m[t].bisect_left(i) - 1, len(m[t]) - 1)
-                if idx >= 0:
-                    ret = min(ret, i - m[t][idx])
-
-        return ret if ret < len(nums) else -1
-
+        for i, num in enumerate(nums):
+            c = (c + num) % p
+            if (c - s + p) % p in d:
+                ret = min(ret, i - d[(c - s + p) % p])
+            d[c] = i
         
 
-        
-
-
-        
+        if ret >= len(nums):
+            return -1
+        return ret
