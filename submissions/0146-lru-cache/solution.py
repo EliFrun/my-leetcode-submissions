095387d99b9d76eb59f55d6c84be1eb87fc76fd2
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-12-05 22:51:31 +0000

    Sync LeetCode submission Runtime - 116 ms (68.89%), Memory - 77.9 MB (28.05%)

--- submissions/0146-lru-cache/solution.py
+++ submissions/0146-lru-cache/solution.py
@@ -1,33 +1,73 @@
+class DeNode:
+    def __init__(self, key, val, left=None, right=None):
+        self.left = left
+        self.right = right
+        self.key = key
+        self.val = val
+
+    def pop_self(self):
+        if self.left:
+            self.left.right = self.right
+        if self.right:
+            self.right.left = self.left
+
+    def left_nav(self):
+        if not self.right:
+            return str(self.val)
+        return str(self.val) + '->' + self.right.left_nav()
+
+    def right_nav(self):
+        if not self.left:
+            return str(self.val)
+        return self.left.right_nav() + '<-' + str(self.val)
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.c = capacity
-        self.t = 0
-        self.times = {}
-        self.q = []
-        self.cache = {}
+        # left is most recent, right is least
+        self.l = DeNode(float('-inf'), float('-inf'))
+        self.r = DeNode(float('-inf'), float('-inf'))
+        self.l.right = self.r
+        self.r.left = self.l
+        self.capacity = capacity
+        self.count = 0
+        self.m = {}
+
+    def push_to_front(self, node):
+        node.pop_self()
+        node.right = self.l.right
+        self.l.right.left = node
+        node.left = self.l
+        self.l.right = node
         
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        node = self.m.get(key, None)
+        if not node:
             return -1
-        self.times[key] = self.t
-        heappush(self.q, (self.t, key))
-        self.t += 1
-        return self.cache[key]
+
+        self.push_to_front(node)
+        return node.val
         
 
     def put(self, key: int, value: int) -> None:
-        if len(self.cache) >= self.c and key not in self.cache:
-            while self.q[0][0] != self.times[self.q[0][1]]:
-                heappop(self.q)
-            t, k = heappop(self.q)
-            self.cache.pop(k)
+        if not self.m.get(key, None):
+            n = DeNode(key, value)
+            self.m[key] = n
+            self.push_to_front(n)
+            self.count += 1
+        else:
+            n = self.m[key]
+            n.val = value
+            self.push_to_front(n)
         
-        self.times[key] = self.t
-        heappush(self.q, (self.t, key))
-        self.t += 1
-        self.cache[key] = value
+        if self.count > self.capacity:
+            v = self.r.left
+            self.m.pop(v.key)
+            self.r.left = v.left
+            v.left.right = self.r
+            self.count -= 1
+
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
