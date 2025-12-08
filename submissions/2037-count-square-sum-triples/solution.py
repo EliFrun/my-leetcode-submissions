498b8030d7c0cffb18f1c7282a94f3719eb495ae
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-12-08 00:16:20 +0000

    Sync LeetCode submission Runtime - 155 ms (60.94%), Memory - 17.9 MB (66.69%)

--- submissions/2037-count-square-sum-triples/solution.py
+++ submissions/2037-count-square-sum-triples/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def countTriples(self, n: int) -> int:
         ret = 0
-        for a in range(1, n - 1):
-            for b in range(a, n):
-                foo = (a ** 2 + b ** 2) ** 0.5
-                if foo <= n and foo.is_integer():
+        for i in range(1, n + 1):
+            for j in range(i + 1, n + 1):
+                v = i ** 2 + j ** 2
+                if sqrt(v) == int(sqrt(v)) and sqrt(v) <= n:
                     ret += 2
-                        
         return ret
-        
