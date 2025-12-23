cc0b360acc491ecaf7ab0a743ab268388118f286
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-12-23 20:06:09 +0000

    Sync LeetCode submission Runtime - 111 ms (13.88%), Memory - 17.2 MB (97.01%)

--- submissions/0264-ugly-number-ii/solution.py
+++ submissions/0264-ugly-number-ii/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        nums = [1]
-        i = 1
-        while i < n:
-            l2 = [ x * 2 for x in nums if x * 2 > nums[-1] ]
-            l3 = [ x * 3 for x in nums if x * 3 > nums[-1] ] 
-            l5 = [ x * 5 for x in nums if x * 5 > nums[-1] ]
-            nums.append(min(l2[0], l3[0], l5[0]))
-            
-            i += 1
+        q = [1]
+        for i in range(n):
+            v = heappop(q)
+            if i == n - 1:
+                return v
+            while q and q[0] == v:
+                heappop(q)
+            heappush(q, 2 * v)
+            heappush(q, 3 * v)
+            heappush(q, 5 * v)
 
-
-        return nums[-1]
-            
+        return -1
 
         
