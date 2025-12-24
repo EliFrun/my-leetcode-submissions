79766d8ddeec834ff6457b42419ae584e934d20e
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-12-24 19:10:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

--- submissions/0685-redundant-connection-ii/solution.py
+++ submissions/0685-redundant-connection-ii/solution.py
@@ -1,58 +1,41 @@
 class Solution:
     def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
-        # find if any node has in-degree == 2
-        indegree = defaultdict(list)
-        bad_node = 0
-        for p, c in edges:
-            indegree[c].append(p)
-            if len(indegree[c]) == 2:
-                bad_node = c
 
-        def parent_set(node):
-            ret = set()
-            curr = node
-            while curr:
-                if curr in ret:
-                    return False
-                ret.add(curr)
-                curr = indegree[curr][0] if len(indegree[curr]) > 0 else None
-            
-            return ret
-        
-        if bad_node > 0:
-            candidates = [
-                indegree[bad_node][0],
-                indegree[bad_node][1]
-            ]
-            a = indegree[bad_node].pop(1)
-            set_left = parent_set(a)
-            set_right = parent_set(bad_node)
-            if set_left == False or set_right == False:
-                return [candidates[0], bad_node]
-            if len(set_left.intersection(set_right)) > 0:
-                return [candidates[1], bad_node]
-            return [candidates[0], bad_node]
+        g = defaultdict(list)
+        for p,c in edges:
+            g[c].append(p)
 
-        # find cycle remove right most entry
-        # @functools.cache
-        def highest_parent(node):
-            nonlocal visited
-            if node in visited:
-                return node
-            visited.add(node)
-            if len(indegree[node]) > 0:
-                return highest_parent(indegree[node][0])
-            return node
+        for k,v in g.items():
+            if len(v) > 1:
+                visited = set()
+                p = v[0]
+                while p:
+                    if p in visited:
+                        return [v[0], k]
+                    visited.add(p)
+                    p = g[p][0] if g[p] else None
+                return [v[1], k]
 
-        for p, c in edges[::-1]:
-            visited = set()
-            a = highest_parent(c)
-            if a == c:
+
+        # find cycle
+        visited = set()
+        curr = 1
+        while curr not in visited:
+            visited.add(curr)
+            curr = g[curr][0]
+
+        main_cycle = set()
+        visited = set()
+        while curr not in visited:
+            visited.add(curr)
+            curr = g[curr][0]
+
+        for p,c in reversed(edges):
+            if p in visited and c in visited:
                 return [p, c]
+        return [-1, -1]
                 
+                
+
             
-
-
-
-        
         
