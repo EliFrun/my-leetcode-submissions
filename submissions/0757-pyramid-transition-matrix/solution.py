5d11703e2da90171406beb7bf031e6972b8158a6
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-12-29 06:29:51 +0000

    Sync LeetCode submission Runtime - 175 ms (61.37%), Memory - 21.3 MB (15.96%)

--- submissions/0757-pyramid-transition-matrix/solution.py
+++ submissions/0757-pyramid-transition-matrix/solution.py
@@ -1,48 +1,30 @@
 class Solution:
     def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
-        d = {}
-        #allowed = ["".join(sorted(list(x))) for x in allowed]
-        f = defaultdict(lambda: defaultdict(set))
-        for a in allowed:
-            f[a[0]][a[1]].add(a[2])
-            
-        allowed = f
-        return self.helper(bottom, allowed, d)
-        
-        
-        
-        
-    def helper(self, bottom, allowed, memo):
-        if bottom in memo:
-            return memo[bottom]
-        if len(bottom) == 1:
-            return True
-        
-        possible = [[] for _ in range(len(bottom) - 1)]
-        
-        for i in range(len(bottom) - 1):
-            a = bottom[i]
-            b = bottom[i + 1]
-            possible[i] = list(allowed[a][b])
-        
-        next_bottoms = possible[0]
-        for i in possible[1:]:
-            a = copy.copy(next_bottoms)
-            foo = []
-            for j in i:
-                foo.extend(x + j for x in a)
-                
-            next_bottoms = foo
-            
-        for i in next_bottoms:
-            if i not in memo:
-                memo[i] = self.helper(i, allowed, memo)
-            if memo[i]:
+
+        pattern_map = defaultdict(set)
+        for s in allowed:
+            pattern_map[s[:2]].add(s[2])
+
+        @cache
+        def solve(layer):
+            if len(layer) == 1:
                 return True
-        
-        return False
-                
-        
-        
-        
+            combos = set([''])
+            for i in range(len(layer) - 1):
+                nxt = set()
+                for s in pattern_map[layer[i:i + 2]]:
+                    for c in combos:
+                        nxt.add(c + s)
+                combos = nxt
+            
+            for combo in combos:
+                if len(combo) < len(layer) - 1:
+                    continue
+                if solve(combo):
+                    return True
+            return False
+
+        return solve(bottom)
+
+
         
