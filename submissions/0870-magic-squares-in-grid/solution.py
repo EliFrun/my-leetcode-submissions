00b0dade8c662942dd819d5301a857daaa514842
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2025-12-30 00:29:45 +0000

    Sync LeetCode submission Runtime - 4 ms (16.18%), Memory - 17.3 MB (79.92%)

--- submissions/0870-magic-squares-in-grid/solution.py
+++ submissions/0870-magic-squares-in-grid/solution.py
@@ -1,32 +1,38 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        def isMagic(arr):
-            if len(list(set(arr[0] + arr[1] + arr[2]))) != 9:
-                return False
-            
-            if not all([1 <= x and x <= 9 for x in arr[0] + arr[1] + arr[2]]):
-                return False
-             
-            return len(
-                    set(
-                        [
-                            sum(arr[0]),
-                            sum(arr[1]),
-                            sum(arr[2]),
-                            sum([x[0] for x in arr]),
-                            sum([x[1] for x in arr]),
-                            sum([x[2] for x in arr]),
-                            sum([arr[0][0], arr[1][1], arr[2][2]]),
-                            sum([arr[2][0], arr[1][1], arr[0][2]])
-                        ]
-                    )
-                ) == 1
         
+        def solve(ii, jj):
+            nums = set()
+            for i in range(ii, ii + 3):
+                for j in range(jj, jj + 3):
+                    if not 1 <= grid[i][j] <= 9:
+                        return 0
+                    if grid[i][j] in nums:
+                        return 0
+                    nums.add(grid[i][j])
+
+            sums = [0] * 8
+            for d in range(3):
+                sums[0] += grid[ii][jj + d]
+                sums[1] += grid[ii + 1][jj + d]
+                sums[2] += grid[ii + 2][jj + d]
+                sums[3] += grid[ii + d][jj]
+                sums[4] += grid[ii + d][jj + 1]
+                sums[5] += grid[ii + d][jj + 2]
+                sums[6] += grid[ii + d][jj + d]
+                sums[7] += grid[ii + 2 - d][jj + d]
+
+            if not all(x == sums[0] for x in sums):
+                return 0
+            return 1
+                    
         ret = 0
         for i in range(len(grid) - 2):
             for j in range(len(grid[0]) - 2):
-                if (isMagic([x[j: j+3] for x in grid[i:i+3]])):
-                    ret += 1
-                    
+                ret += solve(i, j)
+
         return ret
+                
+
+        
         
