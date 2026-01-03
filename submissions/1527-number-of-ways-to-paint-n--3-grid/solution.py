dcc2d0b984f313e3d9aca9db8f003ecce657e6d1
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-03 02:14:58 +0000

    Sync LeetCode submission Runtime - 107 ms (14.55%), Memory - 17.4 MB (38.60%)

--- submissions/1527-number-of-ways-to-paint-n--3-grid/solution.py
+++ submissions/1527-number-of-ways-to-paint-n--3-grid/solution.py
@@ -1,51 +1,49 @@
+M = 1_000_000_007
+
+def matmul(a, b):
+    ret = [[0] * len(b[0]) for _ in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                ret[i][j] = (ret[i][j] + a[i][k] * b[k][j]) % M
+    return ret
+
+def matadd(a,b):
+    ret = [[0] * len(a[0]) for _ in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            ret[i][j] = (a[i][j] + b[i][j]) % M
+    return ret
+
+l = [[0],[1],[2]]
+for i in range(2):
+    new_l = []
+    for i in range(3):
+        for lis in l:
+            if lis[-1] != i:
+                new_l.append(lis + [i])
+    l = new_l
+
+l.sort()
+transition_matrix = [[0] * len(l) for _ in range(len(l))]
+for i in range(len(l)):
+    for j in range(len(l)):
+        if all(l[i][k] != l[j][k] for k in range(3)):
+            transition_matrix[i][j] = 1
+
 class Solution:
     def numOfWays(self, n: int) -> int:
-        # states
-        # 0  RYR
-        # 1  RYG
-        # 2  RGR
-        # 3  RGY
-        # 4  GYG
-        # 5  GYR
-        # 6  GRG
-        # 7  GRY
-        # 8  YRY
-        # 9  YRG
-        # 10 YGY
-        # 11 YGR
-        # 12 total states, need to count state transitions
-        
-        # can compute this
-        states = [
-            'RYR',
-            'RYG',
-            'RGR',
-            'RGY',
-            'GYG',
-            'GYR',
-            'GRG',
-            'GRY',
-            'YRY',
-            'YRG',
-            'YGY',
-            'YGR'
-        ]
-        
-        transition_list = defaultdict(list)
-        for i, state in enumerate(states):
-            for j, state2 in enumerate(states):
-                if all(s1 != s2 for s1, s2 in zip(state, state2)):
-                    transition_list[i].append(j)
-                    
-        curr = [1] * 12
-        
-        for _ in range(n - 1):
-            nxt = [0] * 12
-            for i in range(12):
-                for j in transition_list[i]:
-                    nxt[i] = (nxt[i] + curr[j]) % 1_000_000_007
-                    
-            curr = nxt
-            
-        return sum(curr) % 1_000_000_007
+        t = deepcopy(transition_matrix)
+        ret = [[0] * len(l) for _ in range(len(l))]
+        for i in range(12):
+            ret[i][i] = 1
+        n -= 1
+        while n:
+            if n & 1:
+                ret = matmul(ret, t)
+            t = matmul(t, t)
+            n >>= 1
+        ret = matmul(ret, [[1] for _ in range(12)])
+        return sum(sum(r) for r in ret) % M
+
         
