327e067c74660ebf3a122643f6bcbd0b66c2b896
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-04 19:03:49 +0000

    Sync LeetCode submission Runtime - 314 ms (58.06%), Memory - 18 MB (95.60%)

--- submissions/2325-number-of-ways-to-select-buildings/solution.py
+++ submissions/2325-number-of-ways-to-select-buildings/solution.py
@@ -1,17 +1,21 @@
 class Solution:
     def numberOfWays(self, s: str) -> int:
-        s = [int(c) for c in s]
-        left = [0,0]
-        right = [0,0]
-        for c in s:
-            right[c] += 1
-
         ret = 0
+        l0, l1 = 0, 0
+        r0, r1 = s.count('0'), s.count('1')
         for c in s:
-            right[c] -= 1
-            ret += left[1 - c] * right[1 - c]
-            left[c] += 1
+            if c == '0':
+                r0 -= 1
+            else:
+                r1 -= 1
+
+            
+            if c == '0':
+                ret += l1 * r1
+                l0 += 1
+            else:
+                ret += l0 * r0
+                l1 += 1
 
         return ret
         
-
