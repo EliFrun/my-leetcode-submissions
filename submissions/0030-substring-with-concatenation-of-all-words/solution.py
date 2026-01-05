7d3c702d4b2239fcadec318f8aa6b4d75e5df7ea
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-05 20:53:26 +0000

    Sync LeetCode submission Runtime - 35 ms (42.46%), Memory - 18 MB (92.79%)

--- submissions/0030-substring-with-concatenation-of-all-words/solution.py
+++ submissions/0030-substring-with-concatenation-of-all-words/solution.py
@@ -1,50 +1,25 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        if len(s) == 1:
-            return [0] if s == words[0] and len(words) == 1 else []
-        
+        c = Counter(words)
         ret = []
         for i in range(len(words[0])):
-            ret.extend(self.helper(s[i:], words, i))
-            
-        ret.sort()
-        return ret
-    
-    
-    
-    def helper(self, s, words, shift):
-        q = deque()
-        
-        length = len(words[0])
-        
-        parse = []
-        
-        word = ""
-        for j in s:
-            if len(word) < length:
-                word += j
-            else:
-                parse.append(word)
-                word = j
-                
-        parse.append(word)
-        ret = []  
-        i = 1
-        for word in parse:
-            if len(q) < len(words):
-                if word in q and q.count(word) == words.count(word):
-                    while q.popleft() != word:
-                        continue
-                if word in words:
-                    q.append(word)
+            st = []
+            for j in range(i, len(s), len(words[0])):
+                st.append(s[j:j + len(words[0])])
+            left = 0
+            contains = defaultdict(int)
+            for j, word in enumerate(st):
+                if word in c:
+                    contains[word] += 1
+                    while contains[word] > c[word]:
+                        contains[st[left]] -= 1
+                        left += 1
+                    if all(c[k] == contains[k] for k in c.keys()):
+                        ret.append(i + len(words[0]) * left)
                 else:
-                    q = deque()
-
-            if len(q) == len(words):
-                ret.append((i - len(words)) * length + shift)
-                q.popleft()
-            
-            i += 1
-            
-            
+                    contains = defaultdict(int)
+                    left = j + 1
         return ret
+
+
+        
