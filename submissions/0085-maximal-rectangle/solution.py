eca9f19b9e3c5a566f908bfff2880134f8e345dd
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-11 06:46:46 +0000

    Sync LeetCode submission Runtime - 50 ms (28.55%), Memory - 24.7 MB (44.77%)

--- submissions/0085-maximal-rectangle/solution.py
+++ submissions/0085-maximal-rectangle/solution.py
@@ -1,30 +1,22 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                matrix[i][j] = int(matrix[i][j])
-    
-        def solve(heights):
-            heights.append(0)
-            stack = [-1]
-            ret = 0
-            for i, v in enumerate(heights):
-                while v < heights[stack[-1]]:
-                    curr = stack.pop()
-                    height = heights[curr]
-                    ret = max(ret, height * (i - stack[-1] - 1))
-                stack.append(i)
+        curr = [0] * len(matrix[0])
 
-            return ret
+        ret = 0
+        for row in matrix:
+            for i,v in enumerate(row):
+                curr[i] = curr[i] + 1 if v == "1" else 0
 
-        curr = matrix[0]
-        m = 0
-        i = 0
-        while i < len(matrix):
-            m = max(m, solve(curr))
-            if i + 1 >= len(matrix):
-                break
-            curr = [x + y if y == 1 else 0 for x,y in zip(curr, matrix[i + 1])]
-            i += 1
+            stk = [(-1, 0)]
+            for i, num in enumerate(curr):
+                ret = max(ret, num)
+                last_idx = i
+                while stk and stk[-1][1] >= num:
+                    j,v = stk.pop()
+                    last_idx = j
+                stk.append((last_idx, num))
+                for j,v in stk:
+                    ret = max(ret, (v * (i - j + 1)))
+        
+        return ret
 
-        return m
