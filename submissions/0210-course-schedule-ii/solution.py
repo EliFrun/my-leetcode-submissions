ac964755ba85d9d58b894b3cc4e2bf340f95a5cb
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-13 05:47:04 +0000

    Sync LeetCode submission Runtime - 569 ms (5.02%), Memory - 20.8 MB (33.38%)

--- submissions/0210-course-schedule-ii/solution.py
+++ submissions/0210-course-schedule-ii/solution.py
@@ -1,57 +1,46 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        if not self.canFinish(numCourses, prerequisites):
-            return []
-        
-        course_deps = [[] for i in range(numCourses)]
-        depth = [0 for i in range(numCourses)]
-        
-        for c, p in prerequisites:
-            course_deps[c].append(p)
-        
-        def dfs(c):   
-            nonlocal course_deps
-            nonlocal depth
-            if depth[c] > 0:
-                return depth[c]
-            depth[c] = (1 + max(dfs(p) for p in course_deps[c])) if course_deps[c] else 0
-            return depth[c]
-        
+        indegree = defaultdict(int)
+        g = defaultdict(list)
+        for a,b in prerequisites:
+            g[a].append(b)
+            indegree[b] += 1
+
+
+        # cycle detection
         for i in range(numCourses):
-            dfs(i)
-            
-        return [x[0] for x in sorted(enumerate(depth), key=lambda x: x[1])]
-        
-        
-        
-    
-    
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        course_deps = [[] for i in range(numCourses)]
-        can_reach_base = [0 for i in range(numCourses)]
-        
-        for c, p in prerequisites:
-            course_deps[c].append(p)
-            
-        def dfs(c):
-            nonlocal can_reach_base
-            if can_reach_base[c] == -1:
-                return False
-            if can_reach_base[c] == 1:
-                return True
-            
-            nonlocal course_deps
-            for p in course_deps[c]:
-                can_reach_base[c] = -1
-                if not dfs(p):
-                    return False
-                can_reach_base[c] = 1
-                
-            return True
-            
+            visited = set()
+            curr = set(g[i])
+            while curr:
+                nxt = set()
+                for v in curr:
+                    visited.add(v)
+                    if v == i:
+                        return []
+                    nxt.update(g[v])
+                curr = nxt - visited
+
+
+        visited = [False] * numCourses
+        stk = []
         for i in range(numCourses):
-            if not dfs(i):
-                return False
-            
-        return True
+            if indegree[i]:
+                continue
+            stk.append(i)
+
+        ret = []
+
+        def dfs(n):
+            nonlocal ret
+            if visited[n]:
+                return
+            visited[n] = True
+            for nxt in g[n]:
+                dfs(nxt)
+            ret.append(n)
+
+        for v in stk:
+            dfs(v)
+        return ret
+
         
