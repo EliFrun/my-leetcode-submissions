d9eebc74a592be0d724358b229bae38270b71b3e
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-19 18:00:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (50.30%)

--- submissions/0935-orderly-queue/solution.py
+++ submissions/0935-orderly-queue/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:
-        if k > 1:
-            return ''.join(sorted(s))
-        
-        ret = s
-        for i in range(len(s)):
-            ret = min(ret, s[i:] + s[:i])
-        return ret
-        
+        if k == 1:
+            ret = s
+            for i in range(len(s)):
+                ret = min(ret, s[i:] + s[:i])
+            return ret
+        return ''.join(sorted(list(s)))
