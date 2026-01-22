66253b70ae0859d170b4e59606cf4f0492457d84
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-22 05:38:44 +0000

    Sync LeetCode submission Runtime - 59 ms (6.29%), Memory - 19.8 MB (8.65%)

--- submissions/3773-minimum-pair-removal-to-sort-array-i/solution.py
+++ submissions/3773-minimum-pair-removal-to-sort-array-i/solution.py
@@ -1,55 +1,44 @@
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        q = SortedList()
-        bad_pairs = set()
-        indexes = SortedList(range(len(nums)))
-        for i in range(len(nums) - 1):
-            q.add((nums[i] + nums[i + 1], i, i + 1))
-            if nums[i] > nums[i + 1]:
-                bad_pairs.add((i, i + 1))
 
-        ret = 0
-        while bad_pairs:
-            #print(ret, q, indexes, bad_pairs, nums)
-            v, i, j = q.pop(0)
-            ret += 1
-            if (i, j) in bad_pairs:
-                bad_pairs.remove((i, j))
+        s = SortedList(list(enumerate(nums)))
+        bad_indexes = set()
+        q = []
+        for i in range(len(s) - 1):
+            heappush(q, (s[i + 1][1] + s[i][1], i))
+            if s[i][1] > s[i + 1][1]:
+                bad_indexes.add(i)
 
-            idx_i = indexes.bisect_left(i)
-            idx_j = indexes.bisect_left(j)
-            # left_pair logic
-            if idx_i > 0:
-                l, r = indexes[idx_i - 1], indexes[idx_i]
-                q.remove((nums[l] + nums[r], l, r))
-                if (l,r) in bad_pairs:
-                    bad_pairs.remove((l,r))
-                q.add((nums[l] + v, l, r))
-                if nums[l] > v:
-                    bad_pairs.add((l, r))
-            
-            # right pair logic
-            if idx_j < len(indexes) - 1:
-                l, r = indexes[idx_j], indexes[idx_j + 1]
-                q.remove((nums[l] + nums[r], l, r))
-                if (l,r) in bad_pairs:
-                    bad_pairs.remove((l,r))
-                q.add((nums[r] + v, indexes[idx_i], r))
-                if v > nums[r]:
-                    bad_pairs.add((indexes[idx_i], r))
-            
-            indexes.pop(idx_j)
-            nums[indexes[idx_i]] = v
+        cnt = 0
+        while bad_indexes:
+            d, ii = heappop(q)
+            i = s.bisect_left((ii, float('-inf')))
+            if i >= len(s) - 1:
+                continue
+            if s[i][0] != ii:
+                continue
+            if s[i][1] + s[i + 1][1] != d:
+                continue
+            idx, v = s.pop(i)
+            if idx in bad_indexes:
+                bad_indexes.remove(idx)
+            idx2, v2 = s.pop(i)
+            if idx2 in bad_indexes:
+                bad_indexes.remove(idx2)
+            v += v2
+            s.add((idx, v))
+            if i > 0:
+                if s[i][1] < s[i - 1][1]:
+                    bad_indexes.add(s[i - 1][0])
+                else:
+                    if s[i - 1][0] in bad_indexes:
+                        bad_indexes.remove(s[i - 1][0])
+                heappush(q, (s[i - 1][1] + s[i][1], s[i - 1][0]))
+            if i < len(s) - 1:
+                if s[i][1] > s[i + 1][1]:
+                    bad_indexes.add(s[i][0])
+                heappush(q, (s[i][1] + s[i + 1][1], s[i][0]))
+            cnt += 1
 
-        return ret
-            
-            
-
-
-            
-
-            
-            
-            
-
+        return cnt
         
