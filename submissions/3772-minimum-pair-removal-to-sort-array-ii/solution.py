1a429c45522f894fe38254a9bc43109679f83d47
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-01-23 00:02:02 +0000

    Sync LeetCode submission Runtime - 4579 ms (36.92%), Memory - 75 MB (30.32%)

--- submissions/3772-minimum-pair-removal-to-sort-array-ii/solution.py
+++ submissions/3772-minimum-pair-removal-to-sort-array-ii/solution.py
@@ -1,47 +1,91 @@
+class Node:
+
+    def __init__(self, idx, val, left=None, right=None):
+        self.idx = idx
+        self.val = val
+        self.left = left
+        if self.left:
+            self.left.right = self
+        self.right = right
+
+    def pop_self(self):
+        if self.left:
+            self.left.right = self.right
+        if self.right:
+            self.right.left = self.left
+
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
+        sums = {}
+        bad_indexes = set()
         q = SortedList()
-        bad_pairs = set()
-        indexes = SortedList(range(len(nums)))
-        for i in range(len(nums) - 1):
-            q.add((nums[i] + nums[i + 1], i, i + 1))
-            if nums[i] > nums[i + 1]:
-                bad_pairs.add((i, i + 1))
+        head = Node(-1, -1)
+        curr = head
+        for i, num in enumerate(nums):
+            n = Node(i, num, curr)
+            if i < len(nums) - 1:
+                if num > nums[i + 1]:
+                    bad_indexes.add(i)
+                sums[i] = num + nums[i + 1]
+                q.add((num + nums[i + 1], i, n))
+            curr = n
+        head = head.right
+        head.left = None
 
-        ret = 0
-        while bad_pairs:
-            #print(ret, q, indexes, bad_pairs, nums)
-            v, i, j = q.pop(0)
-            ret += 1
-            if (i, j) in bad_pairs:
-                bad_pairs.remove((i, j))
-
-            idx_i = indexes.bisect_left(i)
-            idx_j = indexes.bisect_left(j)
-            # left_pair logic
-            if idx_i > 0:
-                l, r = indexes[idx_i - 1], indexes[idx_i]
-                q.remove((nums[l] + nums[r], l, r))
-                if (l,r) in bad_pairs:
-                    bad_pairs.remove((l,r))
-                q.add((nums[l] + v, l, r))
-                if nums[l] > v:
-                    bad_pairs.add((l, r))
+        def print_state():
+            print(q, bad_indexes)
+            curr = head
+            while curr:
+                print(curr.val, curr.idx, end=' ')
+                curr = curr.right
+            print()
+        
+        cnt = 0
+        while bad_indexes:
+            #print_state()
             
-            # right pair logic
-            if idx_j < len(indexes) - 1:
-                l, r = indexes[idx_j], indexes[idx_j + 1]
-                q.remove((nums[l] + nums[r], l, r))
-                if (l,r) in bad_pairs:
-                    bad_pairs.remove((l,r))
-                q.add((nums[r] + v, indexes[idx_i], r))
-                if v > nums[r]:
-                    bad_pairs.add((indexes[idx_i], r))
-            
-            indexes.pop(idx_j)
-            nums[indexes[idx_i]] = v
+            s, idx, node = q.pop(0)
 
-        return ret
+            right = node.right
+            if right.idx in bad_indexes:
+                bad_indexes.remove(right.idx)
+            if right.idx in sums:
+                q.remove((sums[right.idx], right.idx, right))
+                sums.pop(right.idx)
+            right.pop_self()
+
+            if idx in bad_indexes:
+                bad_indexes.remove(node.idx)
+
+            node.val = s
+            right = node.right
+            if right:
+                s = node.val + right.val
+                sums[idx] = s
+                if node.val > right.val:
+                    bad_indexes.add(node.idx)
+                q.add((sums[idx], idx, node))
+            else:
+                sums.pop(idx)
+            
+            left = node.left
+            if left:
+                q.remove((sums[left.idx], left.idx, left))
+                sums[left.idx] = left.val + node.val
+                q.add((sums[left.idx], left.idx, left))
+                if left.val > node.val:
+                    bad_indexes.add(left.idx)
+                else:
+                    if left.idx in bad_indexes:
+                        bad_indexes.remove(left.idx)
+
+
+            cnt += 1
+        return cnt
+
+
+
+
             
             
 
