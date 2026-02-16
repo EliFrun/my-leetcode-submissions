7f274b436f0f486ac09f58a9be6fc24a98dea60c
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-02-16 00:54:24 +0000

    Sync LeetCode submission Runtime - 45 ms (62.32%), Memory - 19.2 MB (64.06%)

--- submissions/0190-reverse-bits/solution.py
+++ submissions/0190-reverse-bits/solution.py
@@ -1,15 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        ret = ''
-        for i in range(32):
-            ret += str(n % 2)
-            n = n // 2
-
-        r = 0
-        for i in range(1, 33):
-            r += int(ret[i - 1]) * 2 ** (32 - i)
-
-        return r
-
+        ret = 0
+        for _ in range(32):
+            ret <<= 1
+            ret += n & 1
+            n >>= 1
+        return ret
 
         
