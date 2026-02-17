da3136ddd900216aa35a1e2e141b706bd9b87ff7
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-02-17 01:35:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (83.25%)

--- submissions/0401-binary-watch/solution.py
+++ submissions/0401-binary-watch/solution.py
@@ -2,11 +2,13 @@ class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
         ret = []
         for i in range(12):
-            cnt = bin(i).count('1')
             for j in range(60):
-                if bin(j).count('1') + cnt == turnedOn:
-                    j = ('0' if j < 10 else '') + str(j)
+                if bin(i).count('1') + bin(j).count('1') == turnedOn:
+                    if j < 10:
+                        j = f'0{j}'
                     ret.append(f'{i}:{j}')
 
         return ret
+            
+
         
