436433b271f2e2156a8a6aa527a1430e75c5503a
Eli Frungorts <54901160+EliFrun@users.noreply.github.com>
2026-02-21 04:05:22 +0000

    Sync LeetCode submission Runtime - 31 ms (87.24%), Memory - 31.8 MB (8.60%)

--- submissions/0767-prime-number-of-set-bits-in-binary-representation/solution.py
+++ submissions/0767-prime-number-of-set-bits-in-binary-representation/solution.py
@@ -1,14 +1,27 @@
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
-        primes = set([2,3,5,7,11,13,17,19,23,29])
-        ret = 0
-        for i in range(left, right + 1):
-            a = bin(i)[2:]
-            t = 0
-            for j in a:
-                t += int(j)
-                
-            if t in primes:
-                ret += 1
-                
-        return ret
+        left -= 1
+        primes = [2,3,5,7,11,13,17,19]
+
+        @cache
+        def solve(upper_bound, bits_left):
+            if bits_left == 0:
+                return 1
+            if len(upper_bound) == 1:
+                if upper_bound == '1':
+                    return 1 if bits_left in (0, 1) else 0
+                return 1 if bits_left == 0 else 0
+            
+            if upper_bound[0] == '1':
+                return solve(upper_bound[1:], bits_left - 1) + solve('1' * (len(upper_bound) - 1), bits_left)
+            else:
+                return solve(upper_bound[1:], bits_left)
+
+        left = sum([solve(bin(left)[2:], prime) for prime in primes])
+        right = sum([solve(bin(right)[2:], prime) for prime in primes])
+
+        
+        return right - left
+
+
+        
